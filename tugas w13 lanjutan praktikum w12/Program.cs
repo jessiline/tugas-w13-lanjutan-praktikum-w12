aa6aaea28db7bb5d91977a2aaaa261d1aa9fdd10
jessiline <jimanuela@student.ciputra.ac.id>
2021-12-10 09:18:41 +0700

    Add project files.

---
+++ tugas w13 lanjutan praktikum w12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tugas_w13_lanjutan_praktikum_w12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
